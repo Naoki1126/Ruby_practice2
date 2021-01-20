5fda7286bc66277bbda1574758f25212b39167f7
Naoki Maruyama <m.naoki85@gmail.com>
2021-01-21 00:02:49 +0900

    update convert_hash_syntax.rb

--- cherry_ruby/Regexp.rb
+++ cherry_ruby/Regexp.rb
@@ -51,3 +51,13 @@ text = '123,456-789'
 p text.split(',')
 
 p text.split(/,|-/)
+
+text = '123,456-789'
+
+hash = { ',' => ':', '-' => '/'}
+
+p text.gsub(/,|-/,hash)
+
+text = '123,456-789'
+
+p text.gsub(/,|-/) { |matched| matched == ',' ? ':' : '/'}
