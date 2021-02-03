80ed13f94d9c11d8a0ccaf56ffbe6f5d55369c30
NaokiMaruyama <development@rensa.co.jp>
2021-02-03 09:45:15 +0900

    update method_disclosure_lever.rb

--- cherry_ruby/method_disclosure_level.rb
+++ cherry_ruby/method_disclosure_level.rb
@@ -52,3 +52,160 @@ end
 
 user = User.new
 user.hello #=> Hello, I am Alice #レシーバを指定していないためエラーにならない。多分直接アクセスも出来ない。
+
+
+class Product
+    private
+
+    def name
+        'A great movie'
+    end
+end
+
+class DVD < Product
+
+    def to_s
+        #nameはスーパークラスのprivateメソッド
+        "name: #{name}"
+    end
+end
+
+dvd = DVD.new
+
+dvd.to_s #=> "name: A great movie" #内部でスーパークラスのPrivateメソッドを呼んでいるがエラーにならない
+
+
+class Product
+    def to_s
+        "name: #{name}"
+    end
+    
+    private
+
+    def name
+        'A greate movie'
+    end
+end
+
+class DVD < Product
+    private
+
+    #スーパークラスのprivateメソッドをオーバーライドする
+    def name
+        "An awesome film"
+    end
+end
+
+product = Product.new
+
+#Productクラスのnameメソッドが使われる
+product.to_s #=> "name: A great movie"
+
+dvd = DVD.new
+#オーバーライドしたDVDクラスのnameメソッドが使われる。
+dvd.to_s #=> "name: An awesome film"
+
+
+class User
+    private
+    
+    def self.hello
+        "Hello"
+    end
+end
+
+#クラスメソッドはprivateでもプライベートにならない
+User.hello #=> Hello
+
+
+class User
+    class << self
+    #class << self の構文ならクラスメソッドでもprivateメソッドにできる
+        private
+
+        def hello
+            "Hello"
+        end
+    end
+end
+User.hello #=> NoMethodError
+
+
+class User
+    def self.hello
+        'hello'
+    end
+
+    #後からクラスメソッドをprivateに変更する
+    private_class_method :hello
+end
+
+User.hello #=> NomethodError
+
+
+#after metod disclosure change
+
+class User
+
+    def foo
+        'foo'
+    end
+
+    def bar
+        'bar'
+    end
+
+    #fooとbarをprivateメソッドに変更する
+    #private はメソッドなので引数としてメソッド名を渡すことができる
+    private :foo, :bar
+
+    #bazはpublicメソッド
+
+    def baz
+        'baz'
+    end
+end
+
+user = User.new
+user.foo #=> NomethodError
+user.bar #=> NomethodError
+user.baz #=> baz
+
+
+#protected method  クラス自身とそのサブクラスのインスタンスメソッドからレシーバー付きで呼び出せる
+
+class User
+    #weightは外部に公開しない
+    attr_reader :name
+
+    def initialize(name, weight)
+        @name = name
+        @weight = weight
+    end
+
+    def heavier_than?(other_user)
+        other_user.weight < @weight
+    end
+
+    protected
+
+    #protectedメソッドなので同じクラスかサブクラスであればレシーバ付きで呼び出せる
+    def weight
+        @weight
+    end
+end
+
+alice = User.new('Alice', 50)
+bob = User.new('Bob',60)
+
+#AliceはBobのweightを取得できない
+alice.heavier_than?(bob) #before protected
+#=> NomethodError; undefind method 'weight'
+
+#同じクラスのインスタンスメソッドであればweightが呼び出せる
+alice.heavier_than?(bob) #=> false (after protected)
+bob.heavier_than?(alice) #=> true  (after protected)
+
+#クラスの外ではweightは呼び出せない
+alice.weight #=> NomethodError: proteceted method "weight"
+        
