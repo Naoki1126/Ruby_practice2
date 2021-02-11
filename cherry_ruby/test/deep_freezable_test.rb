e54eb28808558d96f779d7010c06ffeda94c8b3f
Naoki Maruyama <m.naoki85@gmail.com>
2021-02-11 15:07:22 +0900

    finish deep_freezable.rb

--- cherry_ruby/test/deep_freezable_test.rb
+++ cherry_ruby/test/deep_freezable_test.rb
@@ -8,8 +8,21 @@ class DeepFreezableTest < Minitest::Test
         #配列の値は正しいか？
         assert_equal ["Japan","US","India"], Team::COUNTRIES
         #配列自身がfreezeされているか?
-        assert Team::COUNTRIES.fozen?
+        assert Team::COUNTRIES.frozen?
         #配列の要素が全てfreezeさているか?
         assert Team::COUNTRIES.all? { |country| country.frozen?}
+    end
+
+    def test_deep_freeze_to_hash
+        #ハッシュの値は正しいか
+        assert_equal(
+            { 'Japan' => 'yen', 'US' => 'doller', "India" => "rupee"},
+            Bank::CURRNCIES
+        )
+        #ハッシュ自身がfreezeされているか
+        assert Bank::CURRNCIES.frozen?
+
+        #ハッシュの要素(キーと値)が全てfreezeされているか
+        assert Bank::CURRNCIES.all?{ |key,value| key.frozen? && value.frozen? }
     end
 end
