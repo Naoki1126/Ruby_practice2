de87e7f3c92f97431c57865495061ab0896db1f3
Naoki Maruyama <m.naoki85@gmail.com>
2021-03-01 00:00:31 +0900

    update Ruby_2.1.1/Rex/Rex_1.rb

--- Ruby_2.1.1/Rex/Rex_1.rb
+++ Ruby_2.1.1/Rex/Rex_1.rb
@@ -292,32 +292,322 @@
 # #=> hello
 
 
-class C
-  def m1(value)
-    100 + value
-  end
-end
+# class C
+#   def m1(value)
+#     100 + value
+#   end
+# end
 
-module R1
-  refine C do
-    def m1
-      super 50
+# module R1
+#   refine C do
+#     def m1
+#       super 50
+#     end
+#   end
+# end
+
+# module R2
+#   refine C do
+#     def m1
+#       super 100
+#     end
+#   end
+# end
+
+# using R1
+# using R2
+
+# #有効になる再定義は一つだけ
+# #R2が有効になる
+# #super にて親クラスのメソッドに引数　100を渡している
+# puts C.new.m1 #=> 200
+
+
+# class String
+#   # alias_method :hoge, :reverse
+#   alias_method "hoge","reverse"
+#   #alias_methodの引数はシンボルかストリング
+#   # aliasの引数はシンボル
+#   # alias :新メソッド名,:既存メソッド名
+#   #　既存メソッドも使用できる。
+# end
+
+# p "12345".hoge
+# p "321".reverse
+
+
+# class C
+#   def m1
+#     200
+#   end
+# end
+
+# module R
+#   refine C do
+#     def m1
+#       300
+#     end
+#   end
+# end
+
+# using R
+
+# class C
+#   def m1
+#     100
+#   end
+# end
+
+# puts C.new.m1　#=> 300
+
+# #メソッド探索
+# refinement > prependしたモジュール >  Class C > includeしたモジュール　> クラスCのsuperClass
+
+
+# def foo(arg1:100, arg2:200)
+#   puts arg1
+#   puts arg2
+# end
+
+# option = {arg2: 900}
+
+# foo arg1: 200, **option #=>キーワード引数にHashを渡すときは変数の前に*＊をつける。
+
+#=> 200
+#-> 900
+
+
+# class Base
+#   CONST = "Hello, world"
+# end
+
+# class C < Base
+# end
+
+# module P
+#   CONST = "Good, night"
+# end
+
+# class Base
+#   prepend P
+# end
+
+# module M
+#   class C
+#     CONST = "Good, evening"
+#   end
+# end
+
+# module M
+#   class ::C　#=> ::Cはトップレベルにあるものを指す
+#     def greet
+#       CONST
+#     end
+#   end
+# end
+
+# c = C.new
+# p c.class
+# p C.ancestors
+
+# class Object
+#   CONST = "1"
+#   def const_succ
+#     CONST.succ! #=> objectの内容を次に進める
+#   end
+# end
+
+# class Child1
+#   const_succ #2になる
+#   class << self
+#     const_succ #3になる
+#   end
+# end
+
+# class Child2
+#   const_succ  #4になる
+#   def initialize
+#     const_succ #5になる
+#   end
+# end
+
+#定義された段階で、定数の内容が破壊的に変更されている
+# Child1.new
+# p Object::CONST #=> 4
+# Child1.new
+# p Object::CONST #=> 4
+# Child2.new 
+# p Object::CONST #=> 5
+
+# Child2.new 
+# p Object::CONST #=> 6
+
+# p Child1.const_succ #=> 7
+
+
+# class S
+#   def initialize
+#     puts "S#initialize"
+#   end
+# end
+
+# class C < S
+#   def initialize(*args)
+#     super() #=> 引数ありのメソッド内で引数なしのメソッドを呼ぶ場合は明示的に（）を付ける必要がある。
+#     puts "C#initialize"
+#   end
+# end
+
+# C.new(1,2,3,4,5)
+
+
+# p "Matz is my tEacher"[/[J-P]\w+[^ ]/] #=> Matz
+#[J-P] JからPの一文字に一致
+# \w 大文字、小文字、数字、アンダーバーにマッチ
+# + 直前の文字の一回以上の繰り返し
+# [^ ]空白以外
+
+# val = 100
+
+# def method(val)
+#   yield(15 + val)
+# end
+
+# _proc = Proc.new{|arg| val + arg }
+
+# p method(val, &_proc) #=> 215
+# p method(val, &_proc.to_proc) #=> 215
+
+
+# enum = "apple".each_char
+# enum = "apple".to_enum(:each_char)
+# enum = "a p p l e".split(' ').to_enum #=> to_enumはeachなのでレシーバは配列でなければならない
+# enum = "apple".enum_for(:each_char)
+# enum = "a p p l e".split(' ').enum_for #=> enum_forに引数を与えないとto_enumと同じ挙動になる
+# #=> each_char　文字列の各文字に対して繰り返し処理を行う
+
+# p enum.next #=> a
+# p enum.next #=> p
+# p enum.next #=> p
+# p enum.next #=> l
+# p enum.next #=> e
+
+
+# m = Module.new
+
+# m.instance_eval do
+#  p m.instance_variable_set :@block, Module.nesting
+# end
+
+# m.instance_eval(<<-EVAL)
+#   m.instance_variable_set :@eval,  Module.nesting
+# EVAL
+
+# block = m.instance_variable_get :@block
+# puts block
+# _eval =  m.instance_variable_get :@eval
+# puts _eval
+
+# puts block.size
+# puts _eval.size
+
+
+# module M
+#   def foo
+#     super
+#     puts "M#foo"
+#   end
+# end
+
+# class C2
+#   def foo
+#     puts "C2#foo"
+#   end
+# end
+
+# class C < C2
+#   def foo
+#     super
+#     puts "C#foo"
+#   end
+#   include M
+# end
+
+# C.new.foo
+
+# # C2#foo
+# M#foo
+# C#foo
+
+# v1 = 1 / 2 == 0 #=>  true
+# v2 = !!v1 or raise RuntimeError #=> viのtrueにて評価が終わりtrueが入る
+# puts v2 and false　#=> and　左辺が真であれば右辺の結果を返す。左辺が偽であれば左辺を返す
+# #　and は優先順位がputsより低いため trueが返される。
+
+
+# class C
+#   class << self
+#     def hoge
+#       'Hi'
+#     end
+#   end
+#   def self.hoge
+#     'Hi'
+#   end
+#   def hoge
+#     'Goodbye'
+#   end
+# end
+
+
+
+# class C
+#   class << C
+#     def hoge
+#       'Hi'
+#     end
+#   end
+
+#   def self.hoge #=> 普通にオーバーライドされる
+#     'hiHi'
+#   end
+
+#   def hoge
+#     'Goodbye'
+#   end
+# end
+
+# p C.hoge #=> Hi
+# p C.new.hoge #=> Goodbye
+
+
+
+
+class IPAddr
+  include Enumerable
+
+  def initialize(ip_addr)
+    @ip_addr = ip_addr
+  end
+
+  def each
+    return enum_for unless block_given?
+
+    @ip_addr.split('.').each do |octet|
+      yield octet
     end
   end
 end
 
-module R2
-  refine C do
-    def m1
-      super 100
-    end
-  end
-end
+addr = IPAddr.new("192.10.20.30")
+enum = addr.each
 
-using R1
-using R2
+p enum.next # 192と表示される
+p enum.next # 10と表示される
+p enum.next # 20と表示される
+p enum.next # 30と表示される
 
-#有効になる再定義は一つだけ
-#R2が有効になる
-#super にて親クラスのメソッドに引数　100を渡している
-puts C.new.m1 #=> 200
+
+#=> to_enum or enum_for
+
+# question ~30
