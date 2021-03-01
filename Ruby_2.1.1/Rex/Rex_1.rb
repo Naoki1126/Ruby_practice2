8a0bfda8466d2b3daee222598a30adebdcdd4114
Naoki Maruyama <m.naoki85@gmail.com>
2021-03-01 09:38:36 +0900

    update Rex_1.rb

--- Ruby_2.1.1/Rex/Rex_1.rb
+++ Ruby_2.1.1/Rex/Rex_1.rb
@@ -583,31 +583,41 @@
 
 
 
-class IPAddr
-  include Enumerable
+# class IPAddr
+#   include Enumerable
 
-  def initialize(ip_addr)
-    @ip_addr = ip_addr
-  end
+#   def initialize(ip_addr)
+#     @ip_addr = ip_addr
+#   end
 
-  def each
-    return enum_for unless block_given?
+#   def each
+#     return enum_for unless block_given?
 
-    @ip_addr.split('.').each do |octet|
-      yield octet
-    end
-  end
-end
+#     @ip_addr.split('.').each do |octet|
+#       yield octet
+#     end
+#   end
+# end
 
-addr = IPAddr.new("192.10.20.30")
-enum = addr.each
+# addr = IPAddr.new("192.10.20.30")
+# enum = addr.each
 
-p enum.next # 192と表示される
-p enum.next # 10と表示される
-p enum.next # 20と表示される
-p enum.next # 30と表示される
+# p enum.next # 192と表示される
+# p enum.next # 10と表示される
+# p enum.next # 20と表示される
+# p enum.next # 30と表示される
 
 
 #=> to_enum or enum_for
 
 # question ~30
+
+10.times{|d| print d < 2...d > 5 ? "O" : "X" } #=> OOOOOOOXXX
+
+(1..10).each do |d|
+  if d < 2...d > 5
+    puts "O"
+  else
+   puts "X"
+  end
+end
