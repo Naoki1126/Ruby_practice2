d5651ebe5e521297bc11707187d3f7a6e673d82c
Naoki Maruyama <m.naoki85@gmail.com>
2021-03-13 12:00:48 +0900

    update Rex_2.rb

--- Ruby_2.1.1/Rex/Rex_2.rb
+++ Ruby_2.1.1/Rex/Rex_2.rb
@@ -51,17 +51,98 @@
 
 # p M::C.new.awesome_method　#例外が発生
 
-class C
-end
+# class C
+# end
 
-module M
-  CONST = "Hello, world"
+# module M
+#   CONST = "Hello, world"
 
-  C.class_eval do
-    def awesome_method
-      CONST
-    end
-  end
-end
+#   C.class_eval do
+#     def awesome_method
+#       CONST
+#     end
+#   end
+# end
 
-p C.new.awesome_method
+# p C.new.awesome_method
+
+# module M
+#   def class_m
+#     "class_m"
+#   end
+# end
+
+# class C
+#   include M
+# end
+
+# p C.methods.include? :class_m　#=> false
+# p C.new.methods.include? :class_m #=> true
+
+
+# class S
+#   @@val = 0
+#   def initialize
+#     @@val += 1
+#   end
+
+# end
+
+# class C < S
+#   class << C
+#     @@val += 1
+#   end
+
+#   def initialize
+#   end
+# end
+
+# C.new
+# C.new
+# S.new
+# S.new
+
+# p C.class_variable_get(:@@val) #=> 3
+
+# module M1
+
+#   def outputhello
+#     @val = 4
+
+#     puts "hello"
+#   end
+
+# end
+
+
+# module M2
+# end
+
+# class C
+#   include M1
+#   include M2
+# end
+
+# p C.ancestors #=> [C, M2, M1, Object, Kernel, BasicObject]
+
+# module M1
+#   class C1
+#     CONST = "001"
+#   end
+
+#   class C2 < C1
+#     CONST = "010"
+
+#     module M2
+#       CONST = "011"
+
+#       class Ca
+#         CONST = "100"
+#       end
+
+#       class Cb < Ca
+#         p CONST
+#       end
+#     end
+#   end
+# end
