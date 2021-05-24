b5abe4ad1bf51adbead7a42a6965bf0e66bc2f02
Naoki Maruyama <m.naoki85@gmail.com>
2021-05-25 08:44:09 +0900

    update Rex_3

--- Ruby_2.1.1/Rex/Rex_3.rb
+++ Ruby_2.1.1/Rex/Rex_3.rb
@@ -62,10 +62,27 @@
     end
   end
   
-  p C.new.awesome_method
-  p C.new.get_const
-  p C.getted_const
-  p C.getted_c_const
+  p C.new.awesome_method #=> Hello,world class_evalに文字列を用いた場合、モジュールのネストは外れる #class_evalはインスタンスメソッド
+  p C.new.get_const #=> Good Night Class_eval にdoを用いた場合ばレキシカルスコープを参照する
+  p C.getted_const #=> instance_evalはクラスメソッドの追加、レキシカルスコープを探索する。
+  p C.getted_c_const #=> doであってもレキシカルスコープを参照する
+
+  test = C.new
+  test.instance_eval do
+    def test
+      p "ait"
+    end
+  end
+
+  test.test
+
+  test.instance_eval(<<-CODE)
+    def taro
+      puts "taro"
+    end
+  CODE
+
+  test.taro
 
   # class C
   #   CONST = "Hello, world"
@@ -80,3 +97,56 @@
   # end
   
   # p C.new.awesome_method
+
+  # begin
+  #   print "liberty" + :fish.to_s
+  # rescue TypeError
+  #   print "TypeError."
+  # rescue
+  #   print "Error."
+  # else
+  #   print "Else." #=>エラーが起きなかった場合の処理
+  # ensure
+  #   print "Ensure." #=>エラーがあってもなくてもやる処理
+  # end
+
+  # #=> Else Ensure
+
+  # class String
+  #   alias hoge reverse
+  # end
+  
+  # p "12345".hoge
+
+  # class Class
+  #   def method_missing(id, *args)
+  #     puts "Class#method_missing"
+  #   end
+  # end
+  # class A
+  #   def method_missing(id, *args)
+  #     puts "A#method_missing"
+  #   end
+  # end
+  # class B < A
+  #   def method_missing(id, *args)
+  #     puts "B#method_missing"
+  #   end
+  # end
+  
+  # B.new.dummy_method #=> 定義されていないメソッド呼び出しに対しmethod_missingが呼ばれる #B#method_missing
+  # B.dummy_method #=> クラスメソッドのため Class#method_missing
+
+  # class Human
+  #   NAME = "Unknown"
+  
+  #   def name
+  #     NAME
+  #   end
+  # end
+  
+  # class Noguchi < Human
+  #   NAME = "Hideyo"
+  # end
+  
+  # puts Noguchi.new.name #=>定数の参照はレキシカルスコープ
